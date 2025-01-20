561843ba030a2474ce35972f58be06e51cd09b64
Ihuoma <melissagolden56@gmail.com>
2025-01-20 02:42:42 +0100

    mongoose changes

--- server.js
+++ server.js
@@ -13,10 +13,8 @@ app.use(bodyParser.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/blogs', blogRoutes);
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error('Failed to connect to MongoDB:', err));
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => app.listen(process.env.PORT, () => console.log('Server is running')))
+  .catch((err) => console.error('Failed to connect to MongoDB:',err));
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log('Server is running...');
-});
