ca317f6533f385c513d53778336904a0ff8bd1cc
Ihuoma <melissagolden56@gmail.com>
2025-01-20 10:57:04 +0100

    changes

--- server.js
+++ server.js
@@ -13,8 +13,10 @@ app.use(bodyParser.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/blogs', blogRoutes);
 
+const PORT = process.env.PORT || 3000;
+
 mongoose
   .connect(process.env.MONGO_URI)
-  .then(() => app.listen(process.env.PORT, () => console.log('Server is running')))
+  .then(() => app.listen(process.env.PORT, () => console.log(`Server is running on port ${PORT}`)))
   .catch((err) => console.error('Failed to connect to MongoDB:',err));
 
