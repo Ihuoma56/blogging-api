dad563cf9cfd534355f7aa1d4a40fb4a7e4f9f96
Ihuoma <melissagolden56@gmail.com>
2025-01-30 22:07:37 +0100

    Initial commit

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const rateLimiter = require('express-rate-limit');
 const path = require('path')
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
@@ -9,6 +10,13 @@ const connectMongoDB = require('./db')
 dotenv.config();
 
 const app = express();
+
+const limiter = rateLimiter({
+    windowMs : 1 * 60 * 1000,
+    limit : 2,
+    message : {message : 'too many requests'},
+})
+
 app.use(bodyParser.json());
 connectMongoDB(); 
 app.use('/api/auth', authRoutes);
